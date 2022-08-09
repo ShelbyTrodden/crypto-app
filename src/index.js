361adbb8aafeb6d32706db0114a7a68882623616
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-08 20:44:49 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CryptoService from './js/crypto-service.js';
+
+async function getCoin(id) {
+  const response = await CryptoService.getBtc(id);
+  console.log(response);
+  if (response) {
+    printElements(response, id);
+  } else {
+    printError(response);
+  }
+}
+
+function printElements(response, id) {
+  document.querySelector('#showResponse').innerText = `${id} is $ ${response[0].price}`;
+}
+
+function printError(error, id) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${id}: 
+  ${error}.`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const cryptoId = document.querySelector('#id').value;
+  document.querySelector('#id').value = null;
+  getCoin(cryptoId);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
---
+++ src/js/crypto-service.js
@@ -0,0 +1,15 @@
+export default class CryptoService {  
+  static async getBtc(id) {
+    try {
+      const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=${id}&interval=1d,30d&per-page=100&page=1`);
+      const jsonifiedResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return error;
+    }
+  }
+}
